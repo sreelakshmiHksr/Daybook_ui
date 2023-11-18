7c54a3a21eb1b45fea68dbede2973f0aab0b0f23
Harikrishnan S R <sivan.hari.sr@gmail.com>
2023-11-18 21:32:29 +0530

    List item fare changes

--- src/pages/DayBook/components/DayBookList/DayBookList.tsx
+++ src/pages/DayBook/components/DayBookList/DayBookList.tsx
@@ -1,15 +1,28 @@
 import { FC } from "react";
 import { DayBook } from "../../../../models/day-book.model";
 
+import "./DayBookList.scss";
+
 type DayBookListType = {
   items: DayBook[];
 };
 
 const DayBookList: FC<DayBookListType> = ({ items = [] }) => {
   return (
-    <ul>
+    <ul className="day-book-list">
       {items.map((db) => {
-        return <li key={db.id}>{db.date}</li>;
+        return (
+          <li key={db.id} className="day-book-list__item day-book-item">
+            {db.title && (
+              <span className="day-book-item__title">{db.title}</span>
+            )}
+            <div className="day-book-item__date">
+              <span className="pi pi-calendar app-icon"></span>
+              <span>{db.date}</span>
+            </div>
+            <span className="pi pi-angle-double-right day-book-item__goto-detail app-action-icon"></span>
+          </li>
+        );
       })}
     </ul>
   );
